25d9e568896c0be3546fc129c4d4de1ba112bbd4
SkylerRankin <skylerrankin@gmail.com>
2020-08-21 23:45:46 -0400

    Updates

---
+++ precache-manifest.48e73d60c8c71df00c44c92dbc2b1e38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54cbd81c84b781e6b827f88e992286d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89fda3c5fa92853ce7e8",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "167ec591245843796ab8",
+    "url": "/static/css/main.e5cba70e.chunk.css"
+  },
+  {
+    "revision": "89fda3c5fa92853ce7e8",
+    "url": "/static/js/2.44bdf0a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44bdf0a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "167ec591245843796ab8",
+    "url": "/static/js/main.920f4612.chunk.js"
+  },
+  {
+    "revision": "05f69594bdbf2741e2f4",
+    "url": "/static/js/runtime-main.2746d478.js"
+  },
+  {
+    "revision": "433392e9c3539acdd6bb5fca3167ad30",
+    "url": "/static/media/pjd_logo.433392e9.png"
+  }
+]);
